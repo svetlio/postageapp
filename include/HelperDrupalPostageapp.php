8324cf5c5142f2cdfc410bfda9392e26293afc8e
Svetoslav Stoyanov <sve@d-support.eu>
2014-06-26 11:30:14 +0300

    add default PA tpl, subj. and message as vars

--- include/HelperDrupalPostageapp.php
+++ include/HelperDrupalPostageapp.php
@@ -19,66 +19,69 @@ class PostageappDrupalMail extends DefaultMailSystem {
    * Overrides DefaultMailSystem::mail().
    *
    */
-  public function mail(array $message) {    
+  public function mail(array $message) {
     $variables = array();
+
     $variables['submitted'] = array();
     // override body content to PA template
-    $message['body'] = 'sample_child_template';
-    
+    $mail_body = 'sample_child_template';
+
+    $form_id = $_POST['form_id'];
+
     $pa_vars = unserialize(variable_get('postageapp_forms', ''));
-    
+
     // use drupal-default template if sett default
     if (variable_get('postageapp_forms_default', '')) {
-      $message['body'] = 'drupal-default';
+      $mail_body = 'drupal-default';
     }
     else {
-      if (!empty($pa_vars[$fid]['pa_template'])) {
+      if (!empty($pa_vars[$form_id]['pa_template'])) {
         // get Pa tpl for this form
-        $message['body'] = $pa_vars[$_POST['form_id']]['pa_template'];
+        $mail_body = $pa_vars[$form_id]['pa_template'];
       }
     }
-    
+
     // prepare variables for Pa template
-    $variables['submitted'] = postageapp_variables_submitted($_POST);
-    
+    $variables['submitted'] = postageapp_variables_submitted($_POST, $message);
+
     // webform $_POST['submitted']
-    if (strpos($_POST['form_id'], 'webform_client_form_') === 0) {
+    if (strpos($form_id, 'webform_client_form_') === 0) {
       $variables['submitted'] = array_merge($variables['submitted'], $_POST['submitted']);
     }
-    
+
     // add all keys
     $keys = array_keys($variables['submitted']);
     $variables['submitted']['keys'] = implode(', ', $keys);
-    
+
     //
     $to = $message['to'];
     $subject = $message['subject'];
-    $mail_body = $message['body'];
+    //$mail_body = $message['body'];
     $header = array(
       'X-Mailer' => 'Drupal 7',
       'From' => $message['headers']['From'],
       'Reply-to' => $message['headers']['Sender']
     );
-    
+
     //
-    $q = new PostageApp(variable_get('postageapp_api_key', '')); 
+    $q = new PostageApp(variable_get('postageapp_api_key', ''));
     $return = $q->mail(
-      $to, 
+      $to,
       $subject,
       $mail_body,
       $header,
       $variables
     );
-    
+
     if ($return -> response -> status == 'ok') {
       $w_mess = '<br/><b>SUCCESS:</b>, An email was sent and the following response was received: ' . serialize($return -> data -> message);
     } else {
       $w_mess = '<br/><b>Error sending your email:</b> ' . $return -> response -> message;
     }
-    
+
     //
     watchdog ('postageapp', $w_mess);
-    
+
     return TRUE;
   }
 }
